13644c3cfd565e51def09c37f02d75b62dbac2e5
Creamcheesepie <gnldbs1004@naver.com>
2025-09-16 12:31:53 +0900

    유저 아이디와 비밀번호를 대체해서 사용할 UUID 도입, password 필드의 AccessLevel을 PRIVATE로 조정

--- src/main/java/com/rest1/domain/member/member/entity/Member.java
+++ src/main/java/com/rest1/domain/member/member/entity/Member.java
@@ -1,21 +1,30 @@
 package com.rest1.domain.member.member.entity;
 
 import com.rest1.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @Getter
 public class Member extends BaseEntity {
+    @Column(unique = true)
     private String username;
+    @Getter(AccessLevel.PRIVATE)
     private String password;
     private String nickname;
+    @Column(unique = true)
+    private String apiKey;
 
     public Member(String username, String password, String nickname) {
         this.username =username;
         this.password = password;
         this.nickname = nickname;
+        this.apiKey = UUID.randomUUID().toString();
     }
 }
