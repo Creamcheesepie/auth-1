1b261829069f46cb01d0d5b64764c79b2d98bf82
Creamcheesepie <gnldbs1004@naver.com>
2025-09-19 10:24:57 +0900

    standard 패키지에 범용적으로 사용 가능한 Ut 클래스 도입 , JWT 토큰화를 도와주는 클래스 도입

--- src/test/java/com/rest1/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/rest1/domain/member/member/service/AuthTokenServiceTest.java
@@ -1,5 +1,7 @@
 package com.rest1.domain.member.member.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +9,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 public class AuthTokenServiceTest {
+    // 토큰 만료기간: 1년
+    // 만료기간 : 필수
+    private  long expireMillis = 1000L * 60 * 60 * 24 * 365;
+    private byte[] secretPattern = "abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes(StandardCharsets.UTF_8);
+
     @Autowired
     private AuthTokenService authTokenService;
 
@@ -22,5 +34,34 @@ public class AuthTokenServiceTest {
         assertThat(authTokenService).isNotNull();
     }
 
+    @Test
+    @DisplayName("jjwt 최신 방식으로 JWT 생성, {name=/Pual/,age=20}")
+    void t2() {
+
+        // SecretKey : 위변조 방지 토큰, 이 위변조 방지 토큰의 키바이트는 유출되면 안된다.
+        SecretKey secretKey = Keys.hmacShaKeyFor(secretPattern);
+
+        // 발행 시간과 만료 시간 설정
+        Date issuedAt = new Date();
+        Date expiration = new Date(issuedAt.getTime() + expireMillis);
+
+
+        String jwt = Jwts.builder()
+                .claims(Map.of("name", "Paul", "age", 23)) // 내용
+                .issuedAt(issuedAt) // 생성날짜
+                .expiration(expiration) // 만료날짜
+                .signWith(secretKey) // 키 서명
+                .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
+    }
+
+    @Test
+    @DisplayName("Ut.jwt.toString을 통해 JWT 생성, {name=\"Paul\",age=23}")
+    void t3() {
+
+    }
 
 }
